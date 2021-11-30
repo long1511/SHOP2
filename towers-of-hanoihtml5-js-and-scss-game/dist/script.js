15a5ed141fd6855387185354433e7032b99e9033
Long Trương <huymai835@gmail.com>
2021-11-30 21:14:54 +0700

    long

---
+++ towers-of-hanoihtml5-js-and-scss-game/dist/script.js
@@ -0,0 +1,138 @@
+// Alert box using SweetAlert2 - https://limonte.github.io/sweetalert2
+$(document).ready(function() {
+
+	// Variables
+	var holding = [],
+		moves,
+		disksNum = 4,
+		minMoves = 15,
+		$canves = $('#canves'),
+		$restart = $canves.find('.restart'),
+		$tower = $canves.find('.tower'),
+		$scorePanel = $canves.find('#score-panel'),
+		$movesCount = $scorePanel.find('#moves-num'),
+		$ratingStars = $scorePanel.find('i'),
+		rating = 3;
+	
+	// Set Rating and final Score
+	function setRating(moves) {
+		if (moves === 15) {
+			$ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+			rating = 2;
+		} else if (moves >= 16 && moves <= 30) {
+			$ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+			rating = 1;
+		} else if (moves >= 31) {
+			$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
+			rating = 0;
+		}	
+		return { score: rating };
+	};
+
+	// Init Game
+	function initGame(tower) {
+		$tower.html('');
+		moves = 0;
+		$movesCount.html(0);
+		holding = [];
+		for (var i = 1; i <= disksNum; i++) {
+			tower.prepend($('<li class="disk disk-' + i + '" data-value="' + i + '"></li>'));
+		}
+		$ratingStars.each(function() {
+			$(this).removeClass('fa-star-o').addClass('fa-star');
+		});
+	}
+
+	// Game Logic
+	function countMove() {
+		moves++;
+		$movesCount.html(moves);
+
+		if (moves > minMoves - 1) {
+			if ($tower.eq(1).children().length === disksNum || $tower.eq(2).children().length === disksNum) {
+				swal({
+					allowEscapeKey: false,
+					allowOutsideClick: false,
+					title: 'Xin chúc mừng! Bạn đã thắng',				
+					type: 'success',
+					confirmButtonColor: '#8bc34a',
+					confirmButtonText: 'Chơi lại!'
+				}).then(function(isConfirm) {
+					if (isConfirm) {
+						initGame($tower.eq(0));
+					}
+				})
+			}
+		}
+		
+		setRating(moves);
+	}
+
+	function tower(tower) {
+		var $disks = tower.children(); 
+			$topDisk = tower.find(':last-child');
+			topDiskValue = $topDisk.data('value'); 
+			$holdingDisk = $canves.find('.hold');
+		
+		if ($holdingDisk.length !== 0) {
+			if (topDiskValue === holding[0]) {
+				$holdingDisk.removeClass('hold');			
+			} else if (topDiskValue === undefined || topDiskValue > holding[0]) {				
+				$holdingDisk.remove();
+				tower.append($('<li class="disk disk-' + holding[0] + '" data-value="' + holding[0] + '"></li>'));
+				countMove();
+			}
+		} else if ($topDisk.length !== 0) {
+			$topDisk.addClass('hold');
+			holding[0] = topDiskValue;
+		}
+	}
+	
+	function tool(A,C) {		
+		a = $(A.selector + '>li:last').remove();		
+		C.append(a);	
+								
+	}
+	
+	function bot(n,A, B,C)
+	{			
+		if(n==1){
+			tool(A,C); 
+			console.log(A.selector,'=>',C.selector)
+		}
+		else {  
+		bot(n - 1, A, C, B);
+		bot(1,A,B,C);
+		bot(n - 1, B, A, C);
+	}
+}
+	
+	initGame($tower.eq(0));
+	
+	// Event Handlers
+	$canves.on('click', '.tower', function() {
+		var $this = $(this);
+		tower($this);
+	});
+	$('.tool').click(function(){	
+		bot(disksNum,$("#tower-1"),$("#tower-2"),$("#tower-3"));
+	});
+	
+	$restart.on('click', function() {
+		swal({
+				allowEscapeKey: false,
+				allowOutsideClick: false,
+				title: 'Bạn muốn chơi lại?',
+				text: "Tiến trình của bạn sẽ bị mất!",
+				type: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#8bc34a',
+				cancelButtonColor: '#e91e63',
+				confirmButtonText: 'Chơi lại!'
+		}).then(function(isConfirm) {
+				if (isConfirm) {
+					initGame($tower.eq(0));
+				}
+			})
+	});
+});
